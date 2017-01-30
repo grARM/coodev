ecfaed942941e912fa6807f907fdcd3d905800ed
grARM <gricode@126.com>
2017-01-30 19:20:42 +0800

    9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('coodev');
+#!/usr/bin/env node
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author  @grarm
+*/
+var path = require('path');
+
+console.log("coodev 运行的妥妥的");
+
+// var arguments = process.argv.splice(2);
+
+// console.log("arguments:", arguments);
