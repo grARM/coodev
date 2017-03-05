ae6d9340ad3bdf5c44cce80c56612c33354f97a8
grARM <gricode@126.com>
2017-03-05 22:59:50 +0800

    95

--- index.js
+++ index.js
@@ -4,17 +4,57 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author  @grarm
 */
-var path = require('path');
-var _ = require('underscore');
-
-var argMap = require('./config/arg-map.js').argMap;
-
-
-var arguments = process.argv.splice(2);
+// var path = require('path');
+// var _ = require('underscore');
+var program = require('commander');
+var chalk = require('chalk');
 
 
-_.each(arguments, function (v_arg, i_arg){
-	if (!!argMap[v_arg]) {
-		argMap[v_arg]().render();
-	}
-});
+program
+    .version(require('./package').version)
+    .option('-i, --init', '初始化项目文件夹')
+    .option('-d, --dev', '以开发模式启动coodev 即时编译 不压缩')
+    .option('-b, --build', '以生产模式启动coodev 编译一次 压缩')
+    .option('-s, --server', '本地server调试')
+    .option('-p, --publish', '提交到发布仓库')
+    .parse(process.argv);
+
+/** init */
+if (program.init) {
+	require('./src/tasks/task-init.js').render();
+}
+
+/** dev */
+if (program.dev) {
+	require('./src/tasks/task-dev.js').render();
+}
+
+/** build */
+if (program.build) {
+	require('./src/tasks/task-build.js').render();
+}
+
+/** start */
+if (program.start) {
+	require('./src/tasks/task-server.js').render();
+}
+
+/** publish */
+if (program.publish) {
+	require('./src/tasks/task-publish.js').render();
+}
+
+program.on('--help', function () {
+  console.log('  Examples:')
+  console.log()
+  console.log(chalk.gray('    # create a new project with an official template'))
+  console.log('    $ coodev --init')
+  console.log()
+  console.log(chalk.gray('    # develop project'))
+  console.log('    $ coodev --dev')
+  console.log()
+  console.log(chalk.gray('    # start local server'))
+  console.log('    $ coodev --start')
+  console.log()
+})
+
--- src/tasks/task-init.js
+++ src/tasks/task-init.js
@@ -2,6 +2,8 @@ var path = require('path');
 var copyDirs = require('../lib/copy-file.js').copyDir;
 var ROOT_PATH = path.resolve(__dirname);
 var child_process = require('child_process');
+var download = require('download-git-repo');
+var inquirer = require('inquirer');
 // var APP_PATH = path.resolve(ROOT_PATH);
 // var BUILD_PATH = path.resolve(ROOT_PATH, './bin');
 var proPath = process.cwd();
@@ -10,6 +12,20 @@ var templatePath = path.resolve(ROOT_PATH, '../templates');
 var templatePathLocal = path.resolve(templatePath, "./normal/");
 var ora = require('ora');
 var spinnerInit = ora('now is init our porject ...');
+var tmplUrl = 'grARM/coodev-temp-normal';
+
+
+/** get code from github */
+var loadNormalTmpl = function(cb){
+	download(tmplUrl, proPath, function (err) {
+		if(err){
+			console.log('err', err);
+		}
+		cb && cb(err);
+	});
+}
+
+/****/
 
 var fs = require('fs');
 var task = {
@@ -21,19 +37,22 @@ var task = {
 	},
 	'createdir': function(cb){
 		var self = this;
-
-		copyDirs(templatePathLocal, proPath, function (err){
-			if (!!err) {
-				console.log('err: ', err);
-			}
+		loadNormalTmpl(function(){
+			console.log('download templat ok');
 			self.creatDist();
 		});
+		// copyDirs(templatePathLocal, proPath, function (err){
+		// 	if (!!err) {
+		// 		console.log('err: ', err);
+		// 	}
+		// 	self.creatDist();
+		// });
 	},
 	creatDist: function(){
 		var self = this;
 		fs.mkdir(path.resolve(proPath, './dist/'), function(){
 			spinnerInit.stop();
-			console.log('dir creat success now has init 70%');
+			console.log('dir creat success!');
 			console.log('next install lib ...');
 			self.installLib();
 		});
@@ -46,13 +65,27 @@ var task = {
 };
 
 exports.render = function(){
-	spinnerInit.start();
-	task.checkdir(function(exists){
-		if(exists){
-			console.log("this project had init!");
-		}else{
-			task.createdir();
-			//task.creatDist();
-		}
+	inquirer.prompt([{
+	    type: 'list',
+	    message: 'which template do you need:',
+	    name: 'template',
+	    choices: ['normal', 'wap', 'h5']
+	}]).then(function (answers) {
+	  	if (answers.template == 'normal') {
+			tmplUrl = 'grARM/coodev-temp-normal';
+	    }
+
+	    spinnerInit.start();
+		task.checkdir(function(exists){
+			if(exists){
+				console.log("this project had init!");
+			}else{
+				task.createdir();
+				//task.creatDist();
+			}
+		})
+
 	})
+
+	
 };
